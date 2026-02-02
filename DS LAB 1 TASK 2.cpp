647ad923c029b647e1cdf862fed48669e815a8bb
Aleena4560 <aleena123akh@gmail.com>
2026-02-02 20:39:29 +0500

    Add files via upload

---
+++ DS LAB 1 TASK 2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[100], n, index, value;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter array elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter index to update: ";
+    cin >> index;
+
+    cout << "Enter new value: ";
+    cin >> value;
+
+    if (index >= 0 && index < n) {
+        arr[index] = value;
+
+        cout << "Updated array:\n";
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+    } else {
+        cout << "Invalid index";
+    }
+
+    return 0;
+}
